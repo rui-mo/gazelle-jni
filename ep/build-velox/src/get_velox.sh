apache/incubator-gluten

Use Velox provided splitFilters

ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_08_06
+VELOX_REPO=https://github.com/rui-mo/velox.git
+VELOX_BRANCH=extract
 VELOX_HOME=""
 
 OS=`uname -s`

rui-mo/gazelle-jni

ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_11_11
+VELOX_REPO=https://github.com/rui-mo/velox.git
+VELOX_BRANCH=wip_fix
 VELOX_HOME=""
 
 OS=`uname -s`